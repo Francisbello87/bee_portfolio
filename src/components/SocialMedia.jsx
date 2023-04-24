5b965e3b4b0a06d627c5dfccf09d6373ed9dbfd8
Francis Bello <bellofrancis87@gmail.com>
2023-04-24 21:55:52 +0100

    Changes were made to favicon as well as the title

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
-import {BsTwitter, BsInstagram, BsLinkedin} from 'react-icons/bs'
-import {FaFacebookF} from 'react-icons/fa'
+import React from "react";
+import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
+    <div className="app__social">
       <div>
-      <a href="https://twitter.com/bee_nwodu" target="_blank" rel="noreferrer">
-        <BsTwitter/>
-      </a> 
+        <a
+          href="https://twitter.com/bee_nwodu"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
       </div>
       <div>
-      <a href="https://twitter.com/bee_nwodu" target="_blank" rel="noreferrer">
-        <BsInstagram/>
-      </a> 
+        <a
+          href="https://instagram.com/bee_nwodu?igshid=YmMyMTA2M2Y="
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
       </div>
       <div>
-      <a href="https://twitter.com/bee_nwodu" target="_blank" rel="noreferrer">
-        <BsLinkedin/>
-      </a> 
-      </div>  
+        <a
+          href="https://www.linkedin.com/in/blessing-nwodu-564415230"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
